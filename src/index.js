28422da13e9e828c5bb9afb043357991cfbd2716
Salim <53016507+Manou-Codeur@users.noreply.github.com>
2021-12-10 09:36:43 +0100

    Update index.js
    Forgot to wrap the app with the ErrorFetch component

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import Firebase from "./Services/firebase/firebaseConfig";
 import FirebaseContext from "./Services/firebase/firebaseContext";
+import FetchError from "./Components/errorBoundaries/FetchError";
 
 import "./index.scss";
 
@@ -12,11 +13,13 @@ const firebase = new Firebase();
 
 ReactDOM.render(
   <React.StrictMode>
+  <FetchError>
     <BrowserRouter>
       <FirebaseContext.Provider value={firebase}>
         <App />
       </FirebaseContext.Provider>
     </BrowserRouter>
+  </FetchError>
   </React.StrictMode>,
   document.getElementById("root")
 );
